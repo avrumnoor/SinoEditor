c6b04356f21ee548b41b5129a8028904dcfb68e3
Avrum Noor <avrumnoor@stanford.edu>
2021-06-19 16:12:28 +0700

    Initial commit

---
+++ script.js
@@ -0,0 +1,82 @@
+const boldBtn = document.querySelector("#bold-btn")
+const underlineBtn = document.querySelector("#underline-btn")
+const italicBtn = document.querySelector("#italic-btn")
+const colorBtn = document.querySelector("#color-btn")
+
+const newBtn = document.querySelector("#new-btn")
+const txtBtn = document.querySelector("#txt-btn")
+const pdfBtn = document.querySelector("#pdf-btn")
+
+const content = document.querySelector("#content")
+const filename = document.querySelector("#filename-input")
+
+boldBtn.addEventListener("click", () => {
+    document.execCommand("bold")
+})
+
+underlineBtn.addEventListener("click", () => {
+    document.execCommand("underline")
+})
+
+italicBtn.addEventListener("click", () => {
+    document.execCommand("italic")
+})
+
+colorBtn.addEventListener("input", () => {
+    document.execCommand("forecolor", false, colorBtn.value)
+})
+
+newBtn.addEventListener("click", () => {
+    content.innerHTML = ""
+})
+
+txtBtn.addEventListener("click", () => {
+    const a = document.createElement("a")
+    const blob = new Blob([content.innerText])
+    const dataUrl = URL.createObjectURL(blob)
+    a.href = dataUrl
+    a.download = filename.value + ".txt"
+    a.click()
+})
+
+pdfBtn.addEventListener('click', () => {
+    html2pdf().from(content).save(filename.value)
+})
+
+const navToggle = document.querySelector('.nav-toggle');
+const navLinks = document.querySelectorAll('.nav__link')
+
+navToggle.addEventListener('click', () => {
+    document.body.classList.toggle('nav-open');
+});
+
+navLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        document.body.classList.remove('nav-open');
+    })
+})
+
+"use strict";
+
+!function() {
+  var t = window.driftt = window.drift = window.driftt || [];
+  if (!t.init) {
+    if (t.invoked) return void (window.console && console.error && console.error("Drift snippet included twice."));
+    t.invoked = !0, t.methods = [ "identify", "config", "track", "reset", "debug", "show", "ping", "page", "hide", "off", "on" ], 
+    t.factory = function(e) {
+      return function() {
+        var n = Array.prototype.slice.call(arguments);
+        return n.unshift(e), t.push(n), t;
+      };
+    }, t.methods.forEach(function(e) {
+      t[e] = t.factory(e);
+    }), t.load = function(t) {
+      var e = 3e5, n = Math.ceil(new Date() / e) * e, o = document.createElement("script");
+      o.type = "text/javascript", o.async = !0, o.crossorigin = "anonymous", o.src = "https://js.driftt.com/include/" + n + "/" + t + ".js";
+      var i = document.getElementsByTagName("script")[0];
+      i.parentNode.insertBefore(o, i);
+    };
+  }
+}();
+drift.SNIPPET_VERSION = '0.3.1';
+drift.load('n69vmci5b5mn');
